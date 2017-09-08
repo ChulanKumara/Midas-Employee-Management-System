bd37189d26a1f6dbede64683c4a4a7ba9d319187
chulan.kumara <chulan.kumara@DESKTOP-4VO3P04>
2017-09-08 15:58:48 +0530

    initial commit

---
+++ RegistrationAndLogin/Controllers/UserController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Cryptography.Pkcs;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationAndLogin.Models;
+
+namespace RegistrationAndLogin.Controllers
+{
+    public class UserController : Controller
+    {
+        //Registration Action   
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        //Registration POST Action
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude = "IsEmailVerified,ActivationCode")]User user)
+        {
+            bool Status = false;
+            string message = "";
+
+            //Model Validation
+            if (ModelState.IsValid)
+            {
+                #region Email Already Exists
+                var isExist = IsEmailExist(user.Email);
+                if (isExist)
+                {
+                    ModelState.AddModelError("EmailExist", "Email already exist");
+                    return View();
+                }
+                #endregion
+
+                #region Generate Activation Code
+
+                user.ActivationCode = Guid.NewGuid();
+
+                #endregion
+
+                #region Password Hashing
+
+                user.Password = Crypto.Hash(user.Password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword);
+
+                #endregion
+
+                user.IsEmailVerified = false;
+
+                #region Save Data To DB
+
+                using (MidasEMSEntities dc = new MidasEMSEntities())
+                {
+                    dc.Users.Add(user);
+                    dc.SaveChanges();
+
+                    //Send Email To User
+                    SendVerificationLinkEmail(user.Email, user.ActivationCode.ToString());
+                    message = "Registration successfully done. Account activation link " +
+                              " has been sent to your email id:" + user.Email;
+                    Status = true;
+                }
+
+
+                #endregion
+
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return View(user);
+        }
+
+        //Verify Email Account Action
+
+        //Verify Email Link Action
+
+        //Login Action
+
+        //Login POST Action
+
+        //Logout Action
+
+        [NonAction]
+        public bool IsEmailExist(string email)
+        {
+            using (MidasEMSEntities dc = new MidasEMSEntities())
+            {
+                var v = dc.Users.Where(a => a.Email == email).FirstOrDefault();
+                return v != null;
+            }
+        }
+
+        [NonAction]
+        public void SendVerificationLinkEmail(string emailID, string activationCode)
+        {
+            //var scheme = Request.Url.Scheme;
+            //var host = Request.Url.Host;
+            //var port = Request.Url.Port;
+
+            //string url = scheme + "://" + host + "://";
+
+            var verifyUrl = "/User/VerifyAccount" + activationCode;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+
+            var fromEmail = new MailAddress("sappakane@gmail.com", "Midas Safety IT Consultant Trainee");
+            var toEmail = new MailAddress(emailID);
+            var fromEmailPassword = "sappakane@123";
+            string subject = "Your account is successfully created!";
+            string body = "<br/><br/>We are excited to tell you that your account Midas Safety IT Consultant Trainee is" +
+                " successfully created. Please click on the below link to verify your account" +
+                "<br/><br/><a href='"+link+"'>"+link+"</a>";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+
+            smtp.Send(message);
+
+        }
+    }
+}
---
+++ RegistrationAndLogin/Models/Extended/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationAndLogin.Models
+{
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class UserMetadata
+    {
+        [Display(Name ="First Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First Name Required!")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last Name Required!")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email ID")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email ID Required!")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Date Of Birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM-dd-yyyy}")]
+        public DateTime DOB { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password Required!")]
+        [MinLength(6, ErrorMessage = "Minimum 6 characters required")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage = "Passwords do not match!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
